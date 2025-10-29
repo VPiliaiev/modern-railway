89739ba31c8ef3a9321a477870daf2151ce08c59
Valerii <valerapiliaev@gmail.com>
2025-10-29 11:15:54 +0200

    feat: add TicketInLine to Order admin register

--- railway/admin.py
+++ railway/admin.py
@@ -11,11 +11,21 @@ from railway.models import (
     Ticket,
 )
 
+
+class TicketInLine(admin.TabularInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (TicketInLine,)
+
+
 admin.site.register(Station)
 admin.site.register(Route)
 admin.site.register(Crew)
 admin.site.register(Trip)
 admin.site.register(TrainType)
 admin.site.register(Train)
-admin.site.register(Order)
 admin.site.register(Ticket)
